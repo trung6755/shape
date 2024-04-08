8411e3845662bf61ce5d864a31964423f0ad1841
trung6755 <trungggrr@gmail.com>
2024-04-08 09:07:35 +0700

    Initial commit

---
+++ Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace shape
+{
+    public class Circle
+    {
+        private double radius = 1;
+
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double getRadius()
+        {
+            return radius;
+        }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+
+        public double getPerimeter()
+        {
+            return radius * 2 * Math.PI;
+        }
+
+        public override String ToString()
+        {
+            return "A Circle with radius = "
+                    +getRadius()
+                    + ", which is a subclass of "
+                    +base.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+            Console.WriteLine(shape);
+
+            Shape shape2 = new Shape("red", false);
+            Console.WriteLine(shape2);
+
+            Circle circle = new Circle();
+            Console.WriteLine(circle);
+
+            Circle circle2 = new Circle(5);
+            circle2.getArea();
+            circle2.getPerimeter();
+            
+        }
+    }
+}
