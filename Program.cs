fc6fa7016bfac6b092efca05f7380a9e6813cc53
trung6755 <trungggrr@gmail.com>
2024-06-01 11:00:21 +0700

    a

--- Program.cs
+++ Program.cs
@@ -10,38 +10,58 @@ namespace shape
     {
         static void Main(string[] args)
         {
-            Shape shape = new Shape();
-            Console.WriteLine(shape);
+            //Shape shape = new Shape();
+            //Console.WriteLine(shape);
 
-            Shape shape2 = new Shape("red", false);
-            Console.WriteLine(shape2);
+            //Shape shape2 = new Shape("red", false);
+            //Console.WriteLine(shape2);
 
-            Circle circle = new Circle();
-            Console.WriteLine(circle);
+            //Circle circle = new Circle();
+            //Console.WriteLine(circle);
 
-            Circle circle2 = new Circle(5);
-            Console.WriteLine(circle2 + " dien tich la " + circle2.getArea() + ", chu vi la " + circle2.getPerimeter());
+            //Circle circle2 = new Circle(5);
+            //Console.WriteLine(circle2 + " dien tich la " + circle2.getArea() + ", chu vi la " + circle2.getPerimeter());
 
-            ComparableCircle[] circles = new ComparableCircle[1];
-            circles[0] = new ComparableCircle(3.6);
+            //ComparableCircle[] circles = new ComparableCircle[1];
+            //circles[0] = new ComparableCircle(3.6);
             
             
+
+            //Console.WriteLine("Pre-sorted:");
+            //foreach (ComparableCircle circle1 in circles)
+            //{
+            //    Console.WriteLine(circle);
+            //}
+
+            //Array.Sort(circles);
+
+            //Console.WriteLine("After-sorted:");
+            //foreach (ComparableCircle circle1 in circles)
+            //{
+            //    Console.WriteLine(circle);
+            //}
+
+            // CircleComparer Test:
+
+            Circle[] circles1 = new Circle[3];
+            circles1[0] = new Circle(3.6);
+            circles1[1] = new Circle();
+            circles1[2] = new Circle(3.5);
 
             Console.WriteLine("Pre-sorted:");
-            foreach (ComparableCircle circle1 in circles)
+            foreach (Circle circle1 in circles1)
             {
-                Console.WriteLine(circle);
+                Console.WriteLine(circle1);
             }
 
-            Array.Sort(circles);
+            IComparer<Circle> circleComparator = new CircleComparator();
+            Array.Sort(circles1, circleComparator);
 
             Console.WriteLine("After-sorted:");
-            foreach (ComparableCircle circle1 in circles)
+            foreach (Circle circle1 in circles1)
             {
-                Console.WriteLine(circle);
+                Console.WriteLine(circle1);
             }
-
-
 
         }
     }
